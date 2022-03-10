7ca119051492248aaf1c6cfd1b8e6d4146cf317d
root <streamofhope2016@gmail.com>
2022-03-10 14:25:39 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main -entry
+ * main -entry point
+ * programming the first function
  * @void: no argument
  * return : 0 to exit
  */
 int main(void)
 {
-puts("\Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
