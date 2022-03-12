cd4d2d5ea3801575899f7e1e5e52b2d36733bff4
root <streamofhope2016@gmail.com>
2022-03-12 01:47:36 -0800

    commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include  <time.h>
+#include <stdlib.h>
+
+/**
+ * main - This function checks whether a random number assigned to n
+ * is positive or negative
+ * Return: the function returns 0
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive \n" , n);
+}
+else if (n==0)
+{
+printf("%d is zero \n" , n);
+}
+else ("%d print negative \n" , n);
+}
+return (0);
+}
+
