8cba04a381128e83d7e49ccc3f8518e718b6e3b1
root <streamofhope2016@gmail.com>
2022-03-13 00:56:47 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main - this program prints various types of size machines
- 
- * return : this fuunction returns 0
+ * Return : this fuunction returns 0
  */
 int main(void)
 {
@@ -10,5 +9,6 @@ printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float)); return (0);
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
 }	
