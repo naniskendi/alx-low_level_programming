06cf8ff600b3a958cd3842ae1d7536b47cd081cd
root <streamofhope2016@gmail.com>
2022-03-13 00:23:50 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
  * main - main function enter here
- * @void: no urguments
- * description: program starts here and ends here
- 
+ * @void: no urgument
  * return: value 0 to exit
- */
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
