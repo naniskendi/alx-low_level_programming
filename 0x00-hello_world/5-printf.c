437265facfd4e081ccdce9e8548671e854d48015
root <streamofhope2016@gmail.com>
2022-03-12 23:30:16 -0800

    comit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
+ * main - main function enter here
  * print with proper grammer
- * return: 0
- */
+ * return: 0 to exit
+ */ 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
