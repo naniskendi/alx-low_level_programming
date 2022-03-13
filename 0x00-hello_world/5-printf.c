0bf0669453097b29449e0010b1b1ea4c8b48a9ef
root <streamofhope2016@gmail.com>
2022-03-12 23:39:16 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - main function enter here
- * print with proper grammer
- * return: 0 to exit
- */ 
+ * @void: no urguments
+ * description: program starts here and ends here
+ * return: value 0 to exit
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
