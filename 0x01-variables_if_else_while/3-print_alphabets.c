b9bbf5dc706715919cbaf2dafd835dc97710d37c
root <streamofhope2016@gmail.com>
2022-03-14 06:23:21 -0700

    commit 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that writes alphabel in lower and uppercase
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+char n ,m;
+for (n = 'a'; n <= 'z'; n++)
+{
+putchar(n);
+}
+for (m = 'A'; m <= 'z'; m++)
+{
+putchar(m);
+}
+return (0);
+}
