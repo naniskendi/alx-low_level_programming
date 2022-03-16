c506188a86841a94573cde120eec333acff65083
root <streamofhope2016@gmail.com>
2022-03-16 02:25:16 -0700

    tebah

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}	
