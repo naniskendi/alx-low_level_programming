23b89bd5889dad3264c127284d6593d337b669cd
root <streamofhope2016@gmail.com>
2022-03-18 08:44:37 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "nanis"
+
+/**
+ * main - the function starts here
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+_putchar('n');
+_putchar('a');
+_putchar('n');
+_putchar('i');
+_putchar('s');
+_putchar('\n');
+return 0;
+}
+
+
