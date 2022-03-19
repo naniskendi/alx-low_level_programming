36e5fe229725f9907c8e373ba751c884ef22832a
root <streamofhope2016@gmail.com>
2022-03-19 00:00:30 -0700

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,7 +6,8 @@
 int main(void)
 {
 	int i;
-	for(i = 48; i < 58; i++)
+
+	for (i = 48; i < 58; i++)
 	{
 	 	putchar(i);
 	}
@@ -15,5 +16,5 @@ int main(void)
 		putchar(i);
 	}
 	putchar('\n');
-	return(0);
-}	
+	return (0);
+}
