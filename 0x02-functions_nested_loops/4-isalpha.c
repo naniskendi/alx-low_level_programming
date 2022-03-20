0d65a670bd74a7eb4e4c775e3de5f8ff9b25b6db
root <streamofhope2016@gmail.com>
2022-03-20 14:01:56 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * main- main function here
+ * @c: The character to be checked
+ * Return: return 0 
+ */
+int _isalpha(int c)
+{
+	if ((c => 'a' && c <= 'z') || (c => 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{		
+		return (0);
+	}
+}
