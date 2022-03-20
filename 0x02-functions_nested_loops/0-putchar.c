178e3412a1e48f7f363deebf7e1c0b1ddbbc63e0
root <streamofhope2016@gmail.com>
2022-03-20 00:59:29 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - the function starts here
- * Return: return 0 (success)
+ * main - print putchar
+ * Return: 0
  */
 int main(void)
 {
-char txt[8] = "_putchar";
-int i;
-for (i = 0; i < 8; i++)
+	char txt[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
 	{
-	putchar(txt[i]);
+		putchar(txt[i]);
 	}
 	putchar('\n');
-	return 0;
-}
-
-
+	return (0);
+}	
