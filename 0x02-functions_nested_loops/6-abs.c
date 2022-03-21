5dbfb8b1c5742f1e483527419d97553310853905
root <streamofhope2016@gmail.com>
2022-03-21 10:23:47 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - gives absolute value of an integer.
+ *
+ * @i: integer to be passed
+ */
+int _abs(int)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
