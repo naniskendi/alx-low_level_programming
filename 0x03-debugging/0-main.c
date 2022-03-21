36a6121d074c036f669c3291c469c178fc7605ac
root <streamofhope2016@gmail.com>
2022-03-21 12:47:02 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- * main - tests function that prints either positive or negative
+ * main - tests function that prints either positive or negative.
  * Return: 0
  */
 int main(void)
 {
-	inti i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
-	
-	return (0);
-}	
+i = 0;
+
+positive_or_negative(i);
+return (0);
+}
