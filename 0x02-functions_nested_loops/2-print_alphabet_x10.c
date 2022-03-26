cda9078a8353bf14989fdfc10e0e43663aaa275a
naniskendi <streamofhope2016@gmail.com>
2022-03-26 08:51:02 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 97; j < 123; j++)
@@ -18,4 +18,4 @@ void print_alphabet_x10(void)
 		}
 	_putchar('\n');
 	}
-}	
+}
