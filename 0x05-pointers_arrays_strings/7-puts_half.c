768bf65357de5b0a2f87835e4abfea5bc904e7b9
naniskendi <streamofhope2016@gmail.com>
2022-03-26 07:07:48 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - print half string
+ * @str: the string
+ * @str: pointer to string.
+ */
+void puts_half(char *str)
+{
+	int counter = 0, i;
+
+	while (counter >= 0)
+	{
+		if (str[counter] == '\0')
+			break;
+		counter++;
+	}
+
+	if (counter % 2 == 1)
+		i = counter / 2;
+	else
+		i = (counter - 1) / 2;
+
+	for (i++; i < counter; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
