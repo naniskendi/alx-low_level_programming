c3ea133628550a0993c01141868278489076cea8
naniskendi <streamofhope2016@gmail.com>
2022-03-28 01:28:15 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @m: first number
+ * @n: first number
+ *
+ * Return: return the sum.
+ */
+int add(int, int);
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * function that print 9 times starting with 0
+ *
+ * Return : return value 0
+ */
+void times_table(void);
+{
+	
