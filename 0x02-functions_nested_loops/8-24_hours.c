d0d89ad383ac836cbcb5e14ce53cb962bb9a2200
naniskendi <streamofhope2016@gmail.com>
2022-03-28 01:06:25 -0700

    commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+i#include "main.h"
+/**
+ * jack_bauer - prints every minute of 24
+ *
+ * Return: Probably 0
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+		}
+	}
+}
